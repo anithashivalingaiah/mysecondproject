2e3c3a1122192432e4cf60300f0c2ef458def93a
Prashanth Muniraju <pmuniraju@hitachiconsulting.com>
2020-04-14 16:54:24 -0500

    first commit to om

--- export_to_git/src/export_to_git/Git_learning.java
+++ export_to_git/src/export_to_git/Git_learning.java
@@ -5,4 +5,8 @@ public class Git_learning {
 	public static void main(String[]args) {
 		System.out.println("This is for master branch");
 	}
+	
+	public void om() {
+		System.out.println("This is for om branch");
+	}
 }
